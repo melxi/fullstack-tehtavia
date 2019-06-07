5992d9ddd76728cff1c31e0db96c7e377d17b257
Melxi <magomed.futijev@gmail.com>
2019-06-08 00:59:17 +0300

    1.4: kurssitiedot step4

--- osa1/kurssitiedot/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/kurssitiedot/src/index.js
@@ -8,9 +8,7 @@ const Header = props => {
 const Part = props => {
   return (
     <p>
-      {props.part1} {props.exercises1}
-      {props.part2} {props.exercises2}
-      {props.part3} {props.exercises3}
+      {props.parts.name} {props.parts.exercises}
     </p>
   );
 };
@@ -18,9 +16,9 @@ const Part = props => {
 const Content = props => {
   return (
     <>
-      <Part part1={props.part1.name + props.part1.exercises} />
-      <Part part2={props.part2.name + props.part2.exercises} />
-      <Part part3={props.part3.name + props.part3.exercises} />
+      <Part parts={props.parts[0]} />
+      <Part parts={props.parts[1]} />
+      <Part parts={props.parts[2]} />
     </>
   );
 };
@@ -29,31 +27,35 @@ const Total = props => {
   return (
     <p>
       Number of exercises{" "}
-      {props.part1.exercises + props.part2.exercises + props.part3.exercises}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
     </p>
   );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    }
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
