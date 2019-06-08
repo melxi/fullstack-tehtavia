43985d47dda8ae623b73b3924983eac584b9d076
Melxi <magomed.futijev@gmail.com>
2019-06-08 12:34:24 +0300

    1.7: unicafe step2

--- osa1/unicafe/unicafe/src/index.js
+++ osa1/unicafe/unicafe/src/index.js
@@ -6,6 +6,9 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
+  const average = (good * 1 + bad * -1) / all;
+  const positive = (good / all) * 100;
 
   return (
     <div>
@@ -35,6 +38,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average || 0}</p>
+      <p>positive {positive || 0} %</p>
     </div>
   );
 };
