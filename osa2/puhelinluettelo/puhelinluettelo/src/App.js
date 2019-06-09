9453b23ef076d68e5f05e614816446a9ef12312e
Melxi <magomed.futijev@gmail.com>
2019-06-09 23:00:49 +0300

    2.6: puhelinluettelo step1

---
+++ osa2/puhelinluettelo/puhelinluettelo/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleChange = event => {
+    setNewName(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={handleChange} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => (
+          <p key={person.name}>{person.name}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
