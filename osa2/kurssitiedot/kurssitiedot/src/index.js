9319bf9af800547c837b15dde80a205c633d26f2
Melxi <magomed.futijev@gmail.com>
2019-06-09 22:03:56 +0300

    2.4: kurssitiedot step9

--- osa2/kurssitiedot/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/kurssitiedot/src/index.js
@@ -35,46 +35,68 @@ const Total = ({ course }) => {
   );
 };
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
   return (
     <>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      {courses.map((course, index) => (
+        <div key={index}>
+          <Header course={course} />
+          <Content course={course} />
+          <Total course={course} />
+        </div>
+      ))}
     </>
   );
 };
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4
-      }
-    ]
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: "Node.js",
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      <Course courses={courses} />
     </div>
   );
 };
